4f16e8b820d411a5d9d7fe726ff7b7f0898cc7f3
Nagu34 <nagababuchilukuri34@gmail.com>
2019-01-26 09:46:13 -0600

    Create Installations.py

---
+++ lab1/SourceCode/Installations.py
@@ -0,0 +1,11 @@
+#Installation Sourcecode to git shell
+#To configure the username and password with git shell
+$ git config --global user.name "Naxbergo"
+$ git config --global user.email "nagababuchilukuri34@gmail.com"
+#To check the git shell version
+$ git --version
+#To check node and npm version 
+$ node -v
+$ npm -v
+#To install ionic CLI and cordova
+$ npm install -g ionic cordova
