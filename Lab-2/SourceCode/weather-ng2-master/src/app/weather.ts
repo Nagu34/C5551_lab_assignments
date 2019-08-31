1b2c817162d3882060212588a62753a73f133070
Nagu34 <nagababuchilukuri34@gmail.com>
2019-08-30 19:55:13 -0600

    source code

---
+++ Lab-2/SourceCode/weather-ng2-master/src/app/weather.ts
@@ -0,0 +1,8 @@
+export class Weather {
+  constructor(public cityName:string,
+              public temp:string,
+              public type:string,
+              public humidity:string,
+              public pressure:string,
+              public img:string,){}
+}
